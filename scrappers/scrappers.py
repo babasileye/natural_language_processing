87082b66c6ab0439037e2f79446034bfb9098de7
sileye ba <sileye.ba@outlook.com>
2022-05-08 14:11:16 +0200

    updating xml file parser

--- scrappers/scrappers.py
+++ scrappers/scrappers.py
@@ -9,17 +9,30 @@ import pandas as pd
 
 class XmlFileParser:
    """
-   parse xml
+   xml file parser
    """
-   def __init__(xml_file):
+   def __init__(self,xml_file):
       """
+      parse xml file
       """
       self.xml_file=xml_file
       f=open(self.xml_file, "r")
-      tmp_buffer=f.read()
-      self.xml_content=bs(tmp_buffer,"lxml")
+      self.xml_content=bs(f.read(),"lxml")
+      
+   def get_tag_attributes(self,tag_name,attributes_list):
+      """
+      get tag attributes into a pandas dataframe
+      """
+      data=[]
+      tag_data=self.xml_content.find_all(tag_name)
+      for n in range(len(tag_data)):
+         data_buffer={}
+         for attribute in attributes_list:
+            data_buffer[attribute]=tag_data[n].get(attribute)
+         data.append(data_buffer)
+      df=pd.DataFrame.from_dict(data)
+      return df
    
-
 def get_url_content(url,scrapper="article"):
    """
    get url content with article
